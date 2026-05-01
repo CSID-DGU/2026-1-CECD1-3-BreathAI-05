d2dcd072480c32fc0ec16a56d46ae8a7b39415c5
ParkSeoyeon <syn815640@gmail.com>
2026-05-01 18:26:16 +0900

    feat: ttobagi 서버 초기 프로젝트 생성 및 설정

---
+++ ttobagi-server/src/main/java/com/breathAI/ttobagi_server/TtobagiServerApplication.java
@@ -0,0 +1,13 @@
+package com.breathAI.ttobagi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtobagiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtobagiServerApplication.class, args);
+	}
+
+}
